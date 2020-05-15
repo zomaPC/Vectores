e540b1f431598d510fc6826a7f81bdcb310c0f44
Zoma <exelsaga@live.com>
2020-05-15 11:57:40 -0500

    Firts Commit

---
+++ src/main/java/com/mycompany/arreglosuni/UniDimensional.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.arreglosuni;
+
+
+public class UniDimensional {
+ public static void main(String[]args){
+     
+     int numeros[] = new int[5];
+     numeros[0] = 5;
+     numeros[1] = 6;
+     numeros[2] = 7;
+     numeros[3] = 8;
+     numeros[4] = 9;
+     
+     System.out.print(numeros[0] + ",");
+     System.out.print(numeros[1] + ",");
+     System.out.print(numeros[2] + ",");
+     System.out.print(numeros[3] + ",");
+     System.out.print(numeros[4] + ",");
+     
+ }   
+}
